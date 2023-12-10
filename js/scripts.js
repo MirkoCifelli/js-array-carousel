86fd857e6aadc01640c15dc23e38d075e839b4c2
MirkoCifelli <ciffo4@hotmail.it>
2023-12-10 14:01:18 +0100

    Add milestone 3 and loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,62 +2,79 @@
 // Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
 // Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+
+
 const arrayImg = [
-'./img/01.webp',
-'./img/02.webp',
-'./img/03.webp',
-'./img/04.webp',
-'./img/05.webp'
-];
+    '01.webp',
+    '02.webp',
+    '03.webp',
+    '04.webp',
+    '05.webp',
+]   
 
+// creo una array con dentro le img
 
+console.log(arrayImg)
 
+let itemImg = '';
 
-for( let i= 0; i < arrayImg.length; i++) {
-    let itemImg = '';
-    itemImg += `
-                <div class="container-img d-flex align-items-center justify-content-center" >
-                        <div class="items">
-                            <img src="${arrayImg[0]}" alt="">
-                        </div>
-                </div> 
-    `;
-    
-    // if(i != 0 ){
-        
-    // }
-    document.querySelector('main').innerHTML = itemImg;    
-}
+// creo una costante senza valore 
 
+for (let i = 0; i < arrayImg.length; i++){
 
-let onScreen = 0;
+    itemImg += `<div id="album-container" class="album"><img src="./img/${ arrayImg[i] }"></div>`;
+};
 
-// const main = document.querySelector('main');
-// const newDiv = document.createElement('div');
+// la mia costante senza valore la ficco in un ciclo dove assegno e add come valore un elemnto html
 
+let slide = document.querySelector('#album').innerHTML = itemImg;
 
+// creo un variante con assegnazione della variante contente elemnto html e a stampo nel id album
 
-// main.innerHTML += `
-// <div class="container-img d-flex align-items-center justify-content-center" >
-//             <div class="items active">
-//                  <img src="./img/01.webp">
-//             </div>
-//             <div class="items d-none">
-//                  <img src="./img/02.webp">
-//             </div>
-//             <div class="items d-none">
-//                  <img src="./img/03.webp">
-//             </div>
-//             <div class="items d-none">
-//                  <img src="./img/04.webp">
-//             </div>
-//             <div class="items d-none">
-//                  <img src="./img/05.webp">
-//             </div>
-// </div> 
+let next = document.getElementById('next');
+let previous = document.getElementById('previous');
 
-// `
+// creo due varianti per i bottoni che mi faranno scorrere le img nel carosello
 
+let carousel = document.getElementsByClassName("album");
 
+//  ho creato una variante con assegnazione della classe con display-none
 
-// ${arrayImg[i]}
+let counter = 0;
+
+//  creato un contatore con valore zero che mi servirà per slidare le foto nel carosello
+
+carousel[counter].classList.add('visual'); 
+
+// aggiungo alla variante carousel [ con il valore di counter] la classe block
+
+next.addEventListener("click", function(){
+
+    carousel[counter].classList.remove('visual');
+
+    if ( counter < arrayImg.length -1 ){
+
+    counter ++;
+
+    }
+    else{
+
+    counter = 0;
+
+    }
+    carousel[counter].classList.add('visual');
+})
+
+// ho creato un loop infinito con il bottone next e previous in modo tale che lo slider non si fermerà all'ultima img ma ricomincerà da capo
+
+previous.addEventListener("click", function(){
+    carousel[counter].classList.remove('visual');
+
+    if ( counter != 0){
+        counter --;
+    }
+    else{
+        counter = arrayImg.length-1;
+    }
+    carousel[counter].classList.add('visual');
+})
